9d969181b56e2aab4c91a1cdf0a12927bcc3a747
Sergio Ulloa <sergio95rlc@gmail.com>
2018-08-02 12:13:24 +0200

    Proyecto 2 terminado

--- CarML/ViewController.swift
+++ CarML/ViewController.swift
@@ -9,17 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    let cars = Cars()
+    
+    // MARK: IBOutlets
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var segmentedControlModel: UISegmentedControl!
+    @IBOutlet weak var switchExtras: UISwitch!
+    @IBOutlet weak var labelKms: UILabel!
+    @IBOutlet weak var sliderKms: UISlider!
+    @IBOutlet weak var segmentedControlStatus: UISegmentedControl!
+    @IBOutlet weak var labelPrice: UILabel!
+    
+    // MARK: - Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.stackView.setCustomSpacing(25, after: self.segmentedControlModel)
+        self.stackView.setCustomSpacing(25, after: self.switchExtras)
+        self.stackView.setCustomSpacing(25, after: self.sliderKms)
+        self.stackView.setCustomSpacing(50, after: self.segmentedControlStatus)
+        
+        self.calculatePrice()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: IBActions
+    @IBAction func calculatePrice() {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 0
+        
+        let formattedKms = formatter.string(for: self.sliderKms.value) ?? "0"
+        self.labelKms.text = "Kilometraje: \(formattedKms) km"
+        
+        if let prediction = try? cars.prediction(modelo: Double(self.segmentedControlModel.selectedSegmentIndex),
+                                                 extras: self.switchExtras.isOn ? Double(1.0) : Double(0.0),
+                                                 kilometraje: Double(self.sliderKms.value),
+                                                 estado: Double(self.segmentedControlStatus.selectedSegmentIndex)) {
+            let clampValue = max(500, prediction.precio)
+            formatter.numberStyle = .currency
+            
+            self.labelPrice.text = formatter.string(for: clampValue)
+        } else {
+            self.labelPrice.text = "ERROR"
+        }
+    }
 }
-
